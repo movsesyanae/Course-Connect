91b78f8b19c7e6d6a989e0db781777641fbac8ae
gitgood57 <alikmovsesyan@live.com>
2021-01-03 23:32:07 -0500

    Added simple sign in page

---
+++ course_connect/src/Index_Page/SignIn.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect} from 'react';
+
+const SignIn = () => {
+
+    // const [person, setPerson] = useState({ email: '', password: ''})
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChange = (e) => {
+        
+    }
+
+
+    return (
+        <form>
+            <div>
+                <label htmlFor="email">email : </label>
+                <input 
+                    type = 'text' 
+                    id = 'email' 
+                    name = 'email' 
+                    value = {email} 
+                    onChange = { (e) => setEmail(e.target.value) } 
+                    /> 
+            </div>
+
+            <div>
+                <label htmlFor="password">Password : </label>
+                <input type='password' id='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} /> 
+            </div>
+            <div>
+                <button type = 'submit' onClick = { (e) => {e.preventDefault(); console.log(email, password)} }> Login </button>
+            </div>
+        </form>
+    );
+
+    
+}
+
+export default SignIn;
--- course_connect/src/index.js
+++ course_connect/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from '../src/Index_Page/SignIn'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>,
   document.getElementById('root')
 );
