fd4e8492090fceb8dc05a78779ed3ac928b8fc1a
jbarats <jacobarats@gmail.com>
2021-01-09 23:49:19 -0500

    started mobile

---
+++ course_connect/src/SignUp_SignIn_Components/SignUpSliderComponentMobile.js
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import SignIn from './SignIn'
+import './SignInStyle.css'
+import SignUp from './SignUp'
+
+const SignUp_SignIn_Component_Mobile = () => {
+	
+	
+
+    return(
+		<body className = 'entry-page'>
+			<p>pls</p>
+		</body>
+
+    );
+}
+
+export default SignUp_SignIn_Component_Mobile;
--- course_connect/src/index.js
+++ course_connect/src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SignUpSliderComponent from './SignUp_SignIn_Components/SignUpSliderComponent';
+import SignUpSliderComponent from './SignUp_SignIn_Components/SignUpSliderComponent'
+import SignUp_SignIn_Component_Mobile from './SignUp_SignIn_Components/SignUpSliderComponentMobile';
 import CourseSelector from './Course_Selector_Component/CourseSelector';
-
+import {isMobile} from 'react-device-detect';
 
 ReactDOM.render(
   <React.StrictMode>
 
-    <SignUpSliderComponent />
+    {/* <SignUpSliderComponent /> */}
+
+    {isMobile ? <body> <h1> Grace please</h1></body> : <SignUpSliderComponent />}
 
 	{/* <CourseSelector /> */}
 
-    
+    {/* <OnStart/> */}
 
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
